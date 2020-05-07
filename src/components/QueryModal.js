bc2318afb36dc8821338d60d8203c9f02ccbbdf9
Kurtisz Krisztián <kurtisz.krisztian@electool.com>
2020-05-07 12:59:12 +0200

    date selector

--- src/components/QueryModal.js
+++ src/components/QueryModal.js
@@ -2,31 +2,35 @@ import React from 'react';
 import Modal from 'react-modal';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 class QueryModal extends React.Component {
 
   state = {
     
     currencies: [],
-    currNames: {},
+    currFull: {},
     base: "",
-    goal: ""
+    goal: "",
+    rateDate: new Date()
   }
 
   componentDidMount = () => {
     axios.get("https://api.frankfurter.app/currencies")
       .then(res => {
-        const currNames = res.data;
-        const currencies = [...Object.keys(currNames)];
+        const currObject = res.data;
+        const currencies = [...Object.keys(currObject)];
         this.setState({
-          currencies: currencies, currNames: currNames
+          currencies: currencies, currFull: currObject
         });
       })
       .catch(err => console.log(err));
   };
 
   getRates = (base, goal) => {
-    axios.get(`https://api.frankfurter.app/latest?amount=1&from=${base}&to=${goal}`)
+    var dateString = this.state.rateDate.toISOString().substring(0, 10);
+    axios.get(`https://api.frankfurter.app/${dateString}?amount=1&from=${base}&to=${goal}`)
       .then(res => {
         const base = res.data.base
         const date = res.data.date
@@ -40,7 +44,7 @@ class QueryModal extends React.Component {
   };
 
   DropDown = (elem, index) => {
-    return <option key={index} value={elem}>{elem}</option>;
+    return <option key={index} value={index}>{elem}</option>;
   };
 
   handleChangeBase = (e) => {
@@ -51,6 +55,10 @@ class QueryModal extends React.Component {
     this.setState({ goal: e.target.value });
   };
 
+  setDate = (date) => {
+    this.setState({ rateDate: date});
+  };
+
  
   render() {
   return (
@@ -58,27 +66,35 @@ class QueryModal extends React.Component {
       <Modal className=" content modalis mymodal" overlayClassName="myoverlay" isOpen={this.props.openModal} onRequestClose={this.props.hideModal} >
         <h3> How much is the fish? </h3>
         <div>
+        <div>
           <div className="inner">
             <span>Select Base: </span>
             <select className="dropdown-header" name="inputcurrency" autoFocus onChange={this.handleChangeBase}>
               <option>
                 From
               </option>
-              {this.state.currencies.map((c, index) => (this.DropDown(c, index)))}
+              {Object.entries(this.state.currFull).map((pair) => (this.DropDown(pair[1], pair[0])))}
             </select>
+          </div>
+          <div className="inner">
+          <DatePicker
+            selected={this.state.rateDate} onChange={this.setDate}
+             dateFormat="yyyy-MM-dd"
+          />
+          </div>
+          </div>
             <br />
             <span>Select Goal: </span>
             <select className="dropdown-header" name="outputcurrency" autoFocus onChange={this.handleChangeGoal}>
               <option>
                 To
               </option>
-              {this.state.currencies.map((c, index) => (this.DropDown(c, index)))}
+              {Object.entries(this.state.currFull).map((pair) => (this.DropDown(pair[1], pair[0])))}
             </select>
+          
+          <p><br /></p>
+          <div className=" result">{this.props.result ? (<p>{this.props.result}</p>) : (<p><br/></p>)}
           </div>
-          <div className="inner result">
-            {this.props.result}
-          </div>
-          <p><br/></p>
           <div>
             <div className="inner">
               <Button onClick={() => this.getRates(this.state.base, this.state.goal)}>Get Rate</Button>
