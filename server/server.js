31948bd395d42e9b13de3c328353d3a7dce4fc2d
Kurtisz Krisztián <kurtisz.krisztian@electool.com>
2020-05-16 16:27:35 +0200

    start

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
-// const path = require('path');
-// const express = require('express');
-// const app = express();
-// const publicPath = path.join(__dirname, '..', 'public');
+const path = require('path');
+const express = require('express');
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
 
-// app.use(express.static(publicPath));
+app.use(express.static(publicPath));
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(publicPath, 'index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
 
-// app.listen(3000, () => {
-//   console.log('Server is up!');
-// });
+app.listen(3000, () => {
+  console.log('Server is up!');
+});
